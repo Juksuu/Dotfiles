effefa6e9623c35129ea9bfa24b4d469c228bc0b
Frans Paasonen <franspaasonen@gmail.com>
2021-01-04 00:12:18 +0200

    [Fish] Cleanup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,4 @@
-set -x GTK_IM_MODULE ibus
-set -x XMODIFIERS @im=ibus
-set -x QT_IM_MODULE ibus
-set -x GOPATH $HOME/projects/go
-
-set -x PATH $PATH $HOME/.local/bin $HOME/.yarn/bin $HOME/.cargo/bin $HOME/.ghcup/bin /var/lib/flatpak/exports/bin $GOPATH/bin
-
+set -x PATH $PATH $HOME/.local/bin $HOME/.yarn/bin $HOME/.cargo/bin $HOME/.ghcup/bin /var/lib/snapd/snap/bin
 set -x VISUAL nvim
 set -x EDITOR $VISUAL
 
@@ -18,9 +12,3 @@ set -x NVM_DIR ~/.nvm
 nvm use default --silent
 
 alias config='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-
-alias nvimConf='nvim ~/.config/nvim/init.vim'
-alias fishConf='nvim ~/.config/fish/config.fish'
-alias xmonadConf='nvim ~/.xmonad/xmonad.hs'
-alias xmobarConf='nvim ~/.config/xmobar/xmobarrc'
-alias kittyConf='nvim ~/.config/kitty/kitty.conf'
