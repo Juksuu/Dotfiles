67425b454c91722b58d6a0fa9a4aa1f8971f739e
Frans Paasonen <franspaasonen@gmail.com>
2021-01-09 17:32:17 +0200

    [Tslint] Add correct paths for tslint.lua file

--- .config/nvim/lua/efm/tslint.lua
+++ .config/nvim/lua/efm/tslint.lua
@@ -1,5 +1,5 @@
 return {
-    lintCommand = "/home/frans/scripts/tslint.sh ./node_modules/.bin/tslint -s /home/frans/.config/yarn/global/node_modules/tslint-unix-formatter/lib/ -t unix",
+    lintCommand = "$HOME/scripts/tslint.sh ./node_modules/.bin/tslint -s $HOME/.config/yarn/global/node_modules/tslint-unix-formatter/lib/ -t unix",
     lintIgnoreExitCode = true,
     lintStdin = true
 }
