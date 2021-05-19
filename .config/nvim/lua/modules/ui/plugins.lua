cceb7d1d8deb5c6c7dbb89bde4de2bdb06cee580
Frans Paasonen <franspaasonen@gmail.com>
2021-05-20 01:44:36 +0300

    [Alacritty, Fish, Nvim] Change theme to ayu mirage

--- .config/nvim/lua/modules/ui/plugins.lua
+++ .config/nvim/lua/modules/ui/plugins.lua
@@ -1,23 +1,25 @@
 local ui = {}
 local config = require('modules.ui.config')
 
--- ui['mhinz/vim-signify'] = {}
 
 ui['lewis6991/gitsigns.nvim'] = {
     config = config.gitsigns,
-    -- requires = 'nvim-lua/plenary.nvim'
 }
 
 ui['norcalli/nvim-colorizer.lua'] = { 
     config = config.colorizer
 }
 
-ui['folke/tokyonight.nvim'] = {
-    config = config.tokyonight
-}
-
 ui['hoob3rt/lualine.nvim'] = {
     config = config.lualine
+}
+
+--[[ ui['folke/tokyonight.nvim'] = {
+    config = config.tokyonight
+} ]]
+
+ui['Juksuu/neovim-ayu'] = {
+    config = config.ayu
 }
 
 ui['tjdevries/cyclist.vim'] = {}
