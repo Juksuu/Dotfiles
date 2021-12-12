d1598aa6d89a651a916f049a2d09d5412edabba6
Frans Paasonen <franspaasonen@gmail.com>
2021-12-12 22:24:21 +0200

    [Nvim] Fix path in efm tslint config

--- nvim/.config/nvim/lua/plugin_configs/lsp/efm/tslint.lua
+++ nvim/.config/nvim/lua/plugin_configs/lsp/efm/tslint.lua
@@ -1,5 +1,5 @@
 local tslint = {
-    lintCommand = "$HOME/scripts/tslint.sh ./node_modules/.bin/tslint -s $HOME/.config/yarn/global/node_modules/tslint-unix-formatter/lib/ -t unix",
+    lintCommand = "tslint.sh ./node_modules/.bin/tslint -s $HOME/.config/yarn/global/node_modules/tslint-unix-formatter/lib/ -t unix",
     lintIgnoreExitCode = true,
     lintStdin = true,
     lintFormats = { "%f:%l:%c: %m" },
