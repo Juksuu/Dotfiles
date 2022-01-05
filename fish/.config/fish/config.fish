8f114ac8ae8308ecb70fe55969ccbd92093de2c3
Frans Paasonen <franspaasonen@gmail.com>
2022-01-06 00:04:43 +0200

    [Fish] Fix fish dotfiles

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,7 +6,6 @@ set -x CARGO_HOME $HOME/.cargo
 set -x PATH $PATH $HOME/.local/bin $HOME/.yarn/bin $HOME/.cargo/bin $HOME/.ghcup/bin $GOPATH/bin $HOME/.luarocks/bin
 
 set fish_greeting
-# source ~/.config/fish/themes/nordfox.fish
 
 function nvm
     bass source $HOME/.nvm/nvm.sh --no-use ';' nvm $argv
@@ -16,5 +15,3 @@ source /opt/asdf-vm/asdf.fish
 
 set -x NVM_DIR ~/.nvm
 nvm use default --silent
-
-alias config='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
