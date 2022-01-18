83469d55aa2a6449f7a6d71bf82e7054e2b7e521
Frans Paasonen <franspaasonen@gmail.com>
2022-01-18 21:14:09 +0200

    [Nvim] Add pretty fold

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,5 +1,4 @@
 local packer_bootstrap = false
-
 local install_path = vim.fn.stdpath("data")
     .. "/site/pack/packer/start/packer.nvim"
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
@@ -88,6 +87,13 @@ return packer.startup({
             "danymat/neogen",
             event = "BufRead",
             config = require("plugins.configs.neogen"),
+        })
+        use({
+            "anuvyklack/pretty-fold.nvim",
+            config = function()
+                require("pretty-fold").setup({})
+                require("pretty-fold.preview").setup()
+            end,
         })
 
         -- Git
