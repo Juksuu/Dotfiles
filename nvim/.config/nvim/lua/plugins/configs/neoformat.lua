4d540076257f57d5813a3decbbdb93916af33060
Frans Paasonen <franspaasonen@gmail.com>
2022-01-20 16:55:22 +0200

    [Nvim] Fix neoformat

--- nvim/.config/nvim/lua/plugins/configs/neoformat.lua
+++ nvim/.config/nvim/lua/plugins/configs/neoformat.lua
@@ -1,6 +1,7 @@
 return function()
     local utils = require("juksu.utils")
 
+    vim.g.neoformat_try_node_exe = 1
     vim.g.neoformat_only_msg_on_error = true
     vim.g.neoformat_enabled_lua = { "stylua" }
 
