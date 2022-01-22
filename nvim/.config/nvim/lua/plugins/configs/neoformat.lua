67813fb57435c4426745b38c74c768083806d2bc
Frans Paasonen <franspaasonen@gmail.com>
2022-01-22 15:33:09 +0200

    [Nvim] Add commands to enable/disable running neoformat on save

--- nvim/.config/nvim/lua/plugins/configs/neoformat.lua
+++ nvim/.config/nvim/lua/plugins/configs/neoformat.lua
@@ -1,11 +1,28 @@
 return function()
-    local utils = require("juksu.utils")
-
-    vim.g.neoformat_try_node_exe = 1
+    vim.g.neoformat_try_node_exe = true
     vim.g.neoformat_only_msg_on_error = true
     vim.g.neoformat_enabled_lua = { "stylua" }
 
+    -- Configure commands for enabling and disabling run on save
+    vim.g.neoformat_run_on_save = true
+
+    vim.api.nvim_add_user_command("NeoformatDisableOnSave", function()
+        vim.g.neoformat_run_on_save = false
+    end, {})
+
+    vim.api.nvim_add_user_command("NeoformatEnableOnSave", function()
+        vim.g.neoformat_run_on_save = true
+    end, {})
+
+    vim.api.nvim_add_user_command("NeoformatRun", function()
+        if vim.g.neoformat_run_on_save then
+            print(vim.g.neoformat_run_on_save)
+            vim.api.nvim_command("Neoformat")
+        end
+    end, {})
+
+    local utils = require("juksu.utils")
     utils.create_autocommands({
-        format = { { "BufWritePre", "*", "Neoformat" } },
+        format = { { "BufWritePre", "*", "NeoformatRun" } },
     })
 end
