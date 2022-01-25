9a37380180a06478544ebb1dcd933556ad91a283
Frans Paasonen <franspaasonen@gmail.com>
2022-01-25 18:25:59 +0200

    [Nvim] Create custom formatter for 2021 rustfmt for neoformat

--- nvim/.config/nvim/lua/plugins/configs/neoformat.lua
+++ nvim/.config/nvim/lua/plugins/configs/neoformat.lua
@@ -1,7 +1,15 @@
 return function()
     vim.g.neoformat_try_node_exe = true
     vim.g.neoformat_only_msg_on_error = true
+
+    -- Language stuff
     vim.g.neoformat_enabled_lua = { "stylua" }
+    vim.g.neoformat_rust_rustfmt2021 = {
+        exe = "rustfmt",
+        args = { "--edition 2021" },
+        stdin = 1,
+    }
+    vim.g.neoformat_enabled_rust = { "rustfmt2021" }
 
     -- Configure commands for enabling and disabling run on save
     vim.g.neoformat_run_on_save = true
