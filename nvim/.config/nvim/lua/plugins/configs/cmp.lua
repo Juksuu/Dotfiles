1719f3a69c030919d762f005498d867558149c72
Frans Paasonen <frans.paasonen@seepiagames.com>
2022-04-21 12:18:23 +0300

    [Nvim] Fix cmp mapping

--- nvim/.config/nvim/lua/plugins/configs/cmp.lua
+++ nvim/.config/nvim/lua/plugins/configs/cmp.lua
@@ -9,6 +9,8 @@ return function()
             end,
         },
         mapping = {
+            ["<C-n>"] = cmp.mapping.select_next_item(),
+            ["<C-p>"] = cmp.mapping.select_prev_item(),
             ["<C-d>"] = cmp.mapping.scroll_docs(-4),
             ["<C-f>"] = cmp.mapping.scroll_docs(4),
             ["<C-e>"] = cmp.mapping.close(),
