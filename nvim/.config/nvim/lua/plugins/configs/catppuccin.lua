709fa76f9a02ae39f42d81cc9253dd141468282f
Frans Paasonen <franspaasonen@gmail.com>
2022-06-25 21:51:55 +0300

    [Nvim] Adjust catpuccin settings

--- nvim/.config/nvim/lua/plugins/configs/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/configs/catppuccin.lua
@@ -5,10 +5,17 @@ return function()
         term_colors = true,
         styles = {
             comments = "None",
+            conditionals = "None",
+            loops = "None",
             functions = "None",
             keywords = "None",
             strings = "None",
             variables = "None",
+            numbers = "None",
+            booleans = "None",
+            properties = "None",
+            types = "None",
+            operators = "None",
         },
         integrations = {
             treesitter = true,
@@ -21,43 +28,62 @@ return function()
                     information = "None",
                 },
             },
+            coc_nvim = false,
+            lsp_trouble = false,
             cmp = true,
+            lsp_saga = false,
+            gitgutter = false,
             gitsigns = true,
             telescope = true,
             nvimtree = {
                 enabled = false,
                 show_root = false,
+                transparent_panel = false,
             },
+            neotree = {
+                enabled = false,
+                show_root = false,
+                transparent_panel = false,
+            },
+            which_key = false,
             indent_blankline = {
                 enabled = false,
                 colored_indent_levels = false,
             },
             dashboard = false,
             neogit = true,
+            vim_sneak = false,
+            fern = false,
+            barbar = false,
             bufferline = false,
-            notify = true,
+            markdown = false,
+            lightspeed = false,
+            ts_rainbow = false,
+            hop = false,
+            notify = false,
             telekasten = false,
+            symbols_outline = false,
         },
     })
 
     local colors = require("catppuccin.api.colors").get_colors() -- fetch colors with API
     catppuccin.remap({
-        TelescopeNormal = { fg = colors.lavender, bg = colors.black1 },
-        TelescopeBorder = { bg = colors.black1 },
+        TelescopeNormal = { fg = colors.lavender, bg = colors.surface0 },
+        TelescopeBorder = { bg = colors.surface1 },
 
         TelescopeMatching = { fg = colors.red, bg = colors.None },
-        TelescopeSelection = { fg = colors.None, bg = colors.black2 },
+        TelescopeSelection = { fg = colors.None, bg = colors.base },
 
-        TelescopeResultsTitle = { fg = colors.lavender, bg = colors.black1 },
+        TelescopeResultsTitle = { fg = colors.lavender, bg = colors.surface1 },
 
-        TelescopePromptTitle = { fg = colors.rosewater, bg = colors.black3 },
-        TelescopePromptPrefix = { fg = colors.rosewater, bg = colors.black3 },
-        TelescopePromptNormal = { fg = colors.rosewater, bg = colors.black3 },
-        TelescopePromptBorder = { bg = colors.black3 },
+        TelescopePromptTitle = { fg = colors.rosewater, bg = colors.surface2 },
+        TelescopePromptPrefix = { fg = colors.rosewater, bg = colors.surface2 },
+        TelescopePromptNormal = { fg = colors.rosewater, bg = colors.surface2 },
+        TelescopePromptBorder = { bg = colors.surface2 },
 
-        TelescopePreviewTitle = { fg = colors.teal, bg = colors.black2 },
-        TelescopePreviewNormal = { bg = colors.black2 },
-        TelescopePreviewBorder = { bg = colors.black2 },
+        TelescopePreviewTitle = { fg = colors.teal, bg = colors.surface1 },
+        TelescopePreviewNormal = { bg = colors.surface1 },
+        TelescopePreviewBorder = { bg = colors.surface1 },
     })
 
     vim.cmd([[ colorscheme catppuccin ]])
