19232710cae19aa30ff8d3883b3bb26567b575be
Frans Paasonen <franspaasonen@gmail.com>
2022-09-23 16:27:53 +0300

    [Nvim] Use prettier instead of prettierd

--- nvim/.config/nvim/lua/plugins/configs/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/configs/null-ls.lua
@@ -2,7 +2,7 @@ return function()
     require("null-ls").setup({
         sources = {
             require("null-ls").builtins.formatting.stylua,
-            require("null-ls").builtins.formatting.prettierd,
+            require("null-ls").builtins.formatting.prettier,
             require("null-ls").builtins.formatting.rustfmt,
             require("null-ls").builtins.formatting.black,
 
