489051ded8514a678759e11a58b399b857fe2805
Frans Paasonen <franspaasonen@gmail.com>
2022-11-05 18:46:16 +0200

    [Nvim] Removed unused files

--- nvim/.config/nvim/lua/packages/configs/autopairs.lua
+++
@@ -1,5 +0,0 @@
-return function()
-    require("nvim-autopairs").setup({
-        map_cr = true,
-    })
-end
--- nvim/.config/nvim/lua/packages/configs/neogit.lua
+++
@@ -1,4 +0,0 @@
-return function()
-    require("neogit").setup()
-    vim.keymap.set("n", "<leader>gs", require("neogit").open)
-end
