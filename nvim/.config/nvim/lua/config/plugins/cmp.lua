1e68e6e6d89f2e037f385c511d8d084108caa3e5
Frans Paasonen <franspaasonen@gmail.com>
2023-06-06 22:21:01 +0300

    [Nvim] Update packages, fix tabnine config

--- nvim/.config/nvim/lua/config/plugins/cmp.lua
+++ nvim/.config/nvim/lua/config/plugins/cmp.lua
@@ -32,7 +32,16 @@ local M = {
             "tzachar/cmp-tabnine",
             build = "./install.sh",
             config = function()
-                require("cmp_tabnine.config"):setup()
+                local opts = {
+                    max_lines = 1000,
+                    max_num_results = 20,
+                    sort = true,
+                    run_on_every_keystroke = true,
+                    snippet_placeholder = "..",
+                    ignored_file_types = {},
+                    show_prediction_strength = false,
+                }
+                require("cmp_tabnine.config"):setup(opts)
             end,
         },
 
