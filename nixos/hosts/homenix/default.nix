9e369f26e68b064802df9790b44496b074f29c67
Frans Paasonen <franspaasonen@gmail.com>
2024-03-07 19:33:50 +0200

    Remove opengl stuff from nixos config

--- nixos/hosts/homenix/default.nix
+++ nixos/hosts/homenix/default.nix
@@ -87,11 +87,6 @@
   };
 
   hardware = {
-    opengl = {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
-    };
     nvidia = {
       modesetting.enable = true;
       powerManagement.enable = true;
