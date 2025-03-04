3c2bcc6ce127473b676c3ca7b41821ed00155f26
Frans Paasonen <frans.paasonen@seepiagames.com>
2025-03-04 17:26:55 +0200

    [Nvim] Some small fixes

--- configs/nvim/lua/plugins/blink-cmp.lua
+++ configs/nvim/lua/plugins/blink-cmp.lua
@@ -9,7 +9,7 @@ function M.config()
     require("blink.cmp").setup({
         signature = { enabled = true },
         cmdline = {
-            sources = {},
+            enabled = false,
         },
     })
 end
