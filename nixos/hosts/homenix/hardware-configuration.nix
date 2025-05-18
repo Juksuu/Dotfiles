d0570fb7de25044cd516486ea4eff00b056d2628
Frans Paasonen <franspaasonen@gmail.com>
2025-05-18 17:35:05 +0300

    [Nixos] Update homenix hardware-configuration

--- nixos/hosts/homenix/hardware-configuration.nix
+++ nixos/hosts/homenix/hardware-configuration.nix
@@ -8,18 +8,18 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "ahci" "xhci_pci" "thunderbolt" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "ahci" "xhci_pci" "thunderbolt" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/c807aa6b-4ec4-46f7-ae90-6042d800ce03";
+    { device = "/dev/disk/by-uuid/7df74ae0-16b6-4061-9fa1-b9b11eba07b8";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/9501-E3F8";
+    { device = "/dev/disk/by-uuid/5363-C2E6";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
@@ -31,7 +31,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp11s0u2u1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp11s0u1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
