85c7075d000b603290010e71be81bdb26241fffb
Frans Paasonen <franspaasonen@gmail.com>
2025-08-22 19:35:38 +0300

    [Nvim] Load project local config on VimEnter event

--- configs/nvim/plugin/autocommands.lua
+++ configs/nvim/plugin/autocommands.lua
@@ -6,11 +6,23 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 
 -- Source project local nvim configuration
 local PROJECT_CONFIG_FILE_NAME = ".nvim.lua"
+local project_config_augroup = vim.api.nvim_create_augroup("project_config", {})
 vim.api.nvim_create_autocmd("DirChanged", {
+    group = project_config_augroup,
     callback = function(args)
-        print("testing", vim.inspect(args.file))
         local contents = vim.secure.read(
             string.format("%s/%s", args.file, PROJECT_CONFIG_FILE_NAME)
+        )
+        if contents then
+            assert(loadstring(contents))()
+        end
+    end,
+})
+vim.api.nvim_create_autocmd("VimEnter", {
+    group = project_config_augroup,
+    callback = function()
+        local contents = vim.secure.read(
+            string.format("%s/%s", vim.fn.getcwd(), PROJECT_CONFIG_FILE_NAME)
         )
         if contents then
             assert(loadstring(contents))()
