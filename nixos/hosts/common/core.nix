f9143342087b76dcd774676951a10a78fd5d113a
Frans Paasonen <franspaasonen@gmail.com>
2026-02-11 22:36:47 +0200

    [Nixos/Nvim] Update packages

--- nixos/hosts/common/core.nix
+++ nixos/hosts/common/core.nix
@@ -51,7 +51,26 @@
       open = true;
       modesetting.enable = true;
       powerManagement.enable = true;
-      package = config.boot.kernelPackages.nvidiaPackages.beta;
+      # Use custom patch for kernel 6.19 for now until it is added to nixpkgs
+      # package = config.boot.kernelPackages.nvidiaPackages.beta;
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "590.48.01";
+        sha256_64bit = "sha256-ueL4BpN4FDHMh/TNKRCeEz3Oy1ClDWto1LO/LWlr1ok=";
+        sha256_aarch64 = "sha256-FOz7f6pW1NGM2f74kbP6LbNijxKj5ZtZ08bm0aC+/YA=";
+        openSha256 = "sha256-hECHfguzwduEfPo5pCDjWE/MjtRDhINVr4b1awFdP44=";
+        settingsSha256 = "sha256-NWsqUciPa4f1ZX6f0By3yScz3pqKJV1ei9GvOF8qIEE=";
+        persistencedSha256 = "sha256-wsNeuw7IaY6Qc/i/AzT/4N82lPjkwfrhxidKWUtcwW8=";
+        patchesOpen =
+          let
+            kernelPatch = pkgs.fetchpatch {
+              url = "https://raw.githubusercontent.com/CachyOS/CachyOS-PKGBUILDS/refs/heads/master/nvidia/nvidia-utils/kernel-6.19.patch";
+              hash = "sha256-YuJjSUXE6jYSuZySYGnWSNG5sfVei7vvxDcHx3K+IN4=";
+            };
+          in
+          [
+            kernelPatch
+          ];
+      };
     };
     graphics.extraPackages = [ pkgs.nvidia-vaapi-driver ];
   };
